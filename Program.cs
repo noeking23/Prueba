e9f9fd295063cc4f4087f4637c9b942de2cc984a
Noe Magne <Noeca@HARLE_KING.com>
2023-10-24 18:53:49 -0400

    Prueba

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MiProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de números a comparar:");
+            int n = int.Parse(Console.ReadLine());
+
+            int mayor = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ingrese un número:");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > mayor)
+                {
+                    mayor = num;
+                }
+            }
+
+            Console.WriteLine($"El número mayor es: {mayor}");
+        }
+    }
+}
